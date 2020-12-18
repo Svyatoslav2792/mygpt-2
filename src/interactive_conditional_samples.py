cb5087a71f8a38e720afc48c7f94905d9fd5ada5
Svyatoslav2792 <56791053+Svyatoslav2792@users.noreply.github.com>
2020-12-18 15:01:11 +1000

    Update interactive_conditional_samples.py

--- src/interactive_conditional_samples.py
+++ src/interactive_conditional_samples.py
@@ -77,7 +77,7 @@ def interact_model(
             out = sess.run(output, feed_dict={
                 context: [context_tokens for _ in range(batch_size)]
             })[:, len(context_tokens):]
-            text=enc.decode(out[0])
+            text='{{{'+enc.decode(out[0])+'}}}'
             return text
 
 if __name__ == '__main__':
