ab1cde4a4ee4e99da4399fea8c3b7ecfe5e95fac
Svyatoslav2792 <56791053+Svyatoslav2792@users.noreply.github.com>
2020-12-23 15:17:37 +1000

    Update interactive_conditional_samples.py

--- src/interactive_conditional_samples.py
+++ src/interactive_conditional_samples.py
@@ -5,17 +5,15 @@ import json
 import os
 import numpy as np
 import tensorflow as tf
-import sys
-import model, sample, encoder
 
+import model, sample, encoder
 
 def interact_model(
     model_name='124M',
-    raw_text='investment',
     seed=None,
-    nsamples=1,
+    nsamples=1000,
     batch_size=1,
-    length=1024,
+    length=None,
     temperature=1,
     top_k=0,
     top_p=1,
@@ -56,10 +54,10 @@ def interact_model(
     elif length > hparams.n_ctx:
         raise ValueError("Can't get samples longer than window size: %s" % hparams.n_ctx)
 
-    with tf.compat.v1.Session(graph=tf.Graph()) as sess:
-        context = tf.compat.v1.placeholder(tf.int32, [batch_size, None])
+    with tf.Session(graph=tf.Graph()) as sess:
+        context = tf.placeholder(tf.int32, [batch_size, None])
         np.random.seed(seed)
-        tf.compat.v1.set_random_seed(seed)
+        tf.set_random_seed(seed)
         output = sample.sample_sequence(
             hparams=hparams, length=length,
             context=context,
@@ -70,16 +68,24 @@ def interact_model(
         saver = tf.train.Saver()
         ckpt = tf.train.latest_checkpoint(os.path.join(models_dir, model_name))
         saver.restore(sess, ckpt)
-#         raw_text=sys.argv[2]
-        context_tokens = enc.encode(raw_text)
-        generated = 0
-        for _ in range(nsamples // batch_size):
-            out = sess.run(output, feed_dict={
-                context: [context_tokens for _ in range(batch_size)]
-            })[:, len(context_tokens):]
-            text='{{{'+enc.decode(out[0])+'}}}'
-            return text
+
+        while True:
+            raw_text = input("Model prompt >>> ")
+            while not raw_text:
+                print('Prompt should not be empty!')
+                raw_text = input("Model prompt >>> ")
+            context_tokens = enc.encode(raw_text)
+            generated = 0
+            for _ in range(nsamples // batch_size):
+                out = sess.run(output, feed_dict={
+                    context: [context_tokens for _ in range(batch_size)]
+                })[:, len(context_tokens):]
+                for i in range(batch_size):
+                    generated += 1
+                    text = enc.decode(out[i])
+                    print("=" * 40 + " SAMPLE " + str(generated) + " " + "=" * 40)
+                    print(text)
+            print("=" * 80)
 
 if __name__ == '__main__':
     fire.Fire(interact_model)
-
